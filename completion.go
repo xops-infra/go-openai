4a9e87e000f2a09bea4fdf1e33268c49aa54a9ab
lushijie <lushijie@patsnap.com>
2024-07-19 13:46:10 +0800

    chore: add gpt-4o-mini

--- completion.go
+++ completion.go
@@ -24,6 +24,8 @@ const (
 	GPT40314              = "gpt-4-0314"
 	GPT4o                 = "gpt-4o"
 	GPT4o20240513         = "gpt-4o-2024-05-13"
+	GPT4oMini             = "gpt-4o-mini"
+	GPT4oMini20240718     = "gpt-4o-mini-2024-07-18"
 	GPT4Turbo             = "gpt-4-turbo"
 	GPT4Turbo20240409     = "gpt-4-turbo-2024-04-09"
 	GPT4Turbo0125         = "gpt-4-0125-preview"
@@ -85,6 +87,8 @@ var disabledModelsForEndpoints = map[string]map[string]bool{
 		GPT3Dot5Turbo16K0613: true,
 		GPT4:                 true,
 		GPT4o:                true,
+		GPT4oMini:            true,
+		GPT4oMini20240718:    true,
 		GPT4o20240513:        true,
 		GPT4TurboPreview:     true,
 		GPT4VisionPreview:    true,
