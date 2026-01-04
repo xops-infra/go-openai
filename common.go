5e10a183cafd4076a0de8948ef93514ccdbf7425
lushijie <lushijie@patsnap.com>
2026-01-04 13:26:37 +0800

    chore:add empytomit in usage

--- common.go
+++ common.go
@@ -5,26 +5,26 @@ package openai
 // Usage Represents the total token usage per request to OpenAI.
 type Usage struct {
 	PromptTokens            int                      `json:"prompt_tokens"`
-	ImageTokens             int                      `json:"image_tokens"`
+	ImageTokens             int                      `json:"image_tokens,omitempty"`
 	CompletionTokens        int                      `json:"completion_tokens"`
-	ThinkingTokens          int                      `json:"thinking_tokens"`
+	ThinkingTokens          int                      `json:"thinking_tokens,omitempty"`
 	TotalTokens             int                      `json:"total_tokens"`
-	PromptTokensDetails     *PromptTokensDetails     `json:"prompt_tokens_details"`
-	CompletionTokensDetails *CompletionTokensDetails `json:"completion_tokens_details"`
+	PromptTokensDetails     *PromptTokensDetails     `json:"prompt_tokens_details,omitempty"`
+	CompletionTokensDetails *CompletionTokensDetails `json:"completion_tokens_details,omitempty"`
 }
 
 // CompletionTokensDetails Breakdown of tokens used in a completion.
 type CompletionTokensDetails struct {
-	AudioTokens              int `json:"audio_tokens"`
-	ReasoningTokens          int `json:"reasoning_tokens"`
-	AcceptedPredictionTokens int `json:"accepted_prediction_tokens"`
-	RejectedPredictionTokens int `json:"rejected_prediction_tokens"`
+	AudioTokens              int `json:"audio_tokens,omitempty"`
+	ReasoningTokens          int `json:"reasoning_tokens,omitempty"`
+	AcceptedPredictionTokens int `json:"accepted_prediction_tokens,omitempty"`
+	RejectedPredictionTokens int `json:"rejected_prediction_tokens,omitempty"`
 }
 
 // PromptTokensDetails Breakdown of tokens used in the prompt.
 type PromptTokensDetails struct {
-	AudioTokens          int `json:"audio_tokens"`
-	CachedTokens         int `json:"cached_tokens"`
-	CachedCreationTokens int `json:"cached_creation_tokens"` // anthropic specific
-	CacheReadTokens      int `json:"cache_read_tokens"`      // anthropic specific
+	AudioTokens          int `json:"audio_tokens,omitempty"`
+	CachedTokens         int `json:"cached_tokens,omitempty"`
+	CachedCreationTokens int `json:"cached_creation_tokens,omitempty"` // anthropic specific
+	CacheReadTokens      int `json:"cache_read_tokens,omitempty"`      // anthropic specific
 }
